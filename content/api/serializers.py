7a946cbf1efb1749cd306ac283685d48b44b0929
pavlo <pavlo.chubarov.ua@gmail.com>
2020-06-25 18:16:48 +0300

    Update serializer

--- content/api/serializers.py
+++ content/api/serializers.py
@@ -10,12 +10,17 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+
     class Meta:
         model = Post
         fields = ('id', 'author', 'title', 'link', 'author', 'amount_of_upvotes', 'created')
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    post = PostSerializer()
+    author = UserSerializer()
+
     class Meta:
         model = Comment
-        fields = ('id', 'post', 'author', 'created')
+        fields = ('id', 'post', 'author', 'content', 'created')
