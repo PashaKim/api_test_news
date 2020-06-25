2022510fd5548b080a65a4c8660f438ac0b29109
pavlo <pavlo.chubarov.ua@gmail.com>
2020-06-25 17:57:29 +0300

    Add admin

--- content/models.py
+++ content/models.py
@@ -6,11 +6,14 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=250)
     link = models.SlugField(max_length=250, unique=True)
-    upvotes = models.PositiveIntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    @property
+    def amount_of_upvotes(self):
+        return len(self.upvotes.users)
 
     class Meta:
         ordering = ('upvotes', 'created')
@@ -22,9 +25,24 @@ class Comment(models.Model):
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
-    @property
     def __str__(self):
         return f'{self.post}, {self.author}'
+
+    class Meta:
+        ordering = ('created', )
+
+
+class Upvote(models.Model):
+    post = models.OneToOneField(Post, related_name="upvotes", on_delete=models.CASCADE)
+    users = models.ManyToManyField(User, related_name='upvotes')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.post}, {self.amount_of_users}'
+
+    @property
+    def amount_of_users(self):
+        return len(self.users)
 
     class Meta:
         ordering = ('created', )
