023dcbda434699decae99b9a99f6745b64372be8
pavlo <pavlo.chubarov.ua@gmail.com>
2020-06-25 19:14:35 +0300

    Update authentication class

--- content/models.py
+++ content/models.py
@@ -7,7 +7,7 @@ from django.dispatch import receiver
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=250)
-    link = models.CharField(max_length=250, unique=True)
+    link = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
